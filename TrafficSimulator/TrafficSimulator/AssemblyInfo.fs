0be47b0f3a1eaa6ef722fc411b154cfa22679fdc
Remco Visser <rvremco@hotmail.com>
2016-04-25 12:04:53 +0200

    Created project

---
+++ TrafficSimulator/TrafficSimulator/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace TrafficSimulator
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("TrafficSimulator")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("remcovisser")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
