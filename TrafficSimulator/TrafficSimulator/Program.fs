16200ab05e0ae6b5ba782b83ea927ec295a3f521
Remco Visser <rvremco@hotmail.com>
2016-04-25 12:35:28 +0200

    Basic project setup

--- TrafficSimulator/TrafficSimulator/Program.fs
+++ TrafficSimulator/TrafficSimulator/Program.fs
@@ -1,8 +1,8 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open Simulation
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =     
+    use simulation = new TrafficSimulator()
+    simulation.Run()
 
+    0 // return an integer exit code
---
+++ TrafficSimulator/TrafficSimulator/TrafficSimulator.fs
@@ -0,0 +1,30 @@
+﻿module Simulation
+
+open System
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+type TrafficSimulator() as this =
+    inherit Game()
+        
+    do this.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spritebatch = Unchecked.defaultof<SpriteBatch>
+
+    override this.Initialize() = 
+        do base.Initialize()
+   
+    override this.LoadContent() =
+        do spritebatch <- new SpriteBatch(this.GraphicsDevice)
+
+    override this.Update(gameTime) =
+        let deltaTime = gameTime.ElapsedGameTime.TotalSeconds |> float32
+        ()
+
+  
+    override this.Draw(gameTime) =
+        do this.GraphicsDevice.Clear Color.LightGreen       
+        spritebatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+        spritebatch.End() 
+        ()
