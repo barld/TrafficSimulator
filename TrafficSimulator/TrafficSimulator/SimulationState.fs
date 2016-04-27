c7749ccc69ae3f609ead28b87a72dc2709697128
Barld Boot <bakeds@gamil.com>
2016-04-27 21:11:00 +0200

    fixed operator bug

--- TrafficSimulator/TrafficSimulator/SimulationState.fs
+++ TrafficSimulator/TrafficSimulator/SimulationState.fs
@@ -19,16 +19,16 @@ let update dt state =
     let cooldown, vehicle =
         match state.vehicleSpawnCooldown with
         | cooldown when cooldown > 0.f -> state.vehicleSpawnCooldown - dt, []
-        | cooldown when cooldown <= 0.f-> 2.f, [vehicle.TopVehicle; vehicle.RightVehicle; vehicle.BottomVehicle; vehicle.LeftVehicle]
+        | _ -> 3.f, [vehicle.TopVehicle; vehicle.RightVehicle; vehicle.BottomVehicle; vehicle.LeftVehicle]
     {
         state with 
             trafficlights = List.map(TrafficLight.update dt) state.trafficlights
             vehicles = vehicle @ List.map(Vehicle.update dt) state.vehicles 
             |> List.filter(
                 fun vehicle -> 
-                    vehicle.position.Y < 816.f || 
-                    vehicle.position.Y > -16.f || 
-                    vehicle.position.X < 616.f || 
+                    vehicle.position.Y < 816.f && 
+                    vehicle.position.Y > -16.f && 
+                    vehicle.position.X < 616.f &&
                     vehicle.position.X > -16.f
             )
             vehicleSpawnCooldown = cooldown
