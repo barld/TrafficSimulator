312f3eafbb239062f212befd79a613d2c6014b2d
Barld Boot <bakeds@gamil.com>
2016-04-28 14:10:47 +0200

    add exercisses for list function

---
+++ TrafficSimulator/TrafficSimulator/PracticeList.fs
@@ -0,0 +1,17 @@
+﻿module PracticeList
+
+    let rec map: ('a -> 'b) -> list<'a> -> list<'b> =
+        fun mapper list ->
+            failwith "not implemented"
+
+    let rec filter: ('a -> bool) -> list<'a> -> list<'a> = 
+        fun predicate list ->
+            failwith "not implemented"
+
+    let rec tryFind: ('a -> bool) -> list<'a> -> Option<'a> =
+        fun predicate list ->
+            failwith "not implemented"
+
+    let rec iter: ('a -> Unit) -> list<'a> -> Unit =
+        fun f list ->
+            failwith "not implemented"
